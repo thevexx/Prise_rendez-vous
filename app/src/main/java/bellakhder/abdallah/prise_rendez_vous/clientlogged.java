e57014a09248ec92198c8f6eb682dccd815d797c
chehir <abe.crunck@gmail.com>
2017-05-24 02:32:13 +0100

    2nd commit

--- app/src/main/java/bellakhder/abdallah/prise_rendez_vous/clientlogged.java
+++ app/src/main/java/bellakhder/abdallah/prise_rendez_vous/clientlogged.java
@@ -1,17 +1,29 @@
 package bellakhder.abdallah.prise_rendez_vous;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -114,15 +126,9 @@ public class clientlogged extends AppCompatActivity implements View.OnClickListe
     }
 
     public void initList(){
+        httpRequest("http://meet.ili-studios.tn/ws1/Admin/","GET","");
 
-        items=new String[]{"DR1","DR2","DR3","DR4"};
 
-        listItems=new ArrayList<>(Arrays.asList(items));
-
-        adapter=new ArrayAdapter<String>(this,
-                R.layout.list_item, R.id.txtitem, listItems);
-
-        listView.setAdapter(adapter);
 
     }
 
@@ -131,4 +137,81 @@ public class clientlogged extends AppCompatActivity implements View.OnClickListe
         Intent i = new Intent(clientlogged.this, listrdvclient.class);
         startActivity(i);
     }
+
+
+    public void httpRequest(String Url, String method, String xmldata) {
+
+        new AsyncTask<String, String, String>() {
+            String method;
+            int tmp;
+            String data="";
+
+            protected void onPreExecute() {
+            }
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    URL url = new URL(params[0]);
+                    method = params[1];
+                    String urlParams = params[2];
+
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    //httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setRequestMethod(method);
+
+//                    OutputStream os = httpURLConnection.getOutputStream();
+//                    os.write(urlParams.getBytes());
+//                    os.flush();
+//                    os.close();
+
+                    InputStream is = httpURLConnection.getInputStream();
+                    while((tmp=is.read())!=-1){
+                        data+= (char)tmp;
+                    }
+
+                    is.close();
+                    httpURLConnection.disconnect();
+
+
+
+                    return data;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    return "Exception: "+e.getMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "Exception: "+e.getMessage();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                Log.v("TEST",msg);
+
+                ///
+               // items=new String[]{"DR1","DR2","DR3","DR00000"};
+                listItems=new ArrayList<>();
+
+                try {
+                    JSONArray jsonArray = new JSONArray(msg);
+                    for(int i = 0;i<jsonArray.length();i++){
+                        listItems.add(jsonArray.getJSONObject(i).getString("profession_admin"));
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                adapter=new ArrayAdapter<String>(clientlogged.this,R.layout.list_item, R.id.txtitem, listItems);
+
+                listView.setAdapter(adapter);
+            }
+        }.execute(Url, method, xmldata);
+    }
+
+
 }
