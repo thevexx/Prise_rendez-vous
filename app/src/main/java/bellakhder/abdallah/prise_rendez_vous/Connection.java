6b725d0ff1933ce3232a04622a16234109b7f687
chehir <abe.crunck@gmail.com>
2017-05-24 02:19:02 +0100

    2nd commit

--- app/src/main/java/bellakhder/abdallah/prise_rendez_vous/Connection.java
+++ app/src/main/java/bellakhder/abdallah/prise_rendez_vous/Connection.java
@@ -1,8 +1,73 @@
 package bellakhder.abdallah.prise_rendez_vous;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Administrator on 5/20/2017.
  */
 
 public class Connection {
+
+    public static void httpRequest(String Url, String method, String xmldata) {
+
+        new AsyncTask<String, String, String>() {
+            String method;
+            int tmp;
+            String data="";
+
+            protected void onPreExecute() {
+            }
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    URL url = new URL(params[0]);
+                    method = params[1];
+                    String urlParams = params[2];
+
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    //httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setRequestMethod(method);
+
+                    OutputStream os = httpURLConnection.getOutputStream();
+                    os.write(urlParams.getBytes());
+                    os.flush();
+                    os.close();
+
+                    InputStream is = httpURLConnection.getInputStream();
+                    while((tmp=is.read())!=-1){
+                        data+= (char)tmp;
+                    }
+
+                    is.close();
+                    httpURLConnection.disconnect();
+
+                    return data;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    return "Exception: "+e.getMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "Exception: "+e.getMessage();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                Log.v("TEST",msg);
+
+            }
+        }.execute(Url, method, xmldata);
+    }
+
 }
